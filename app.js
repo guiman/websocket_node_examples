56c1414b9d9ad1923e2506cb9a55a88796056cb3
Alvaro F. Lara <alvarola@gmail.com>
2013-02-17 12:18:59 -0300

    Allowing message and user save [in development]

--- app.js
+++ app.js
@@ -15,7 +15,50 @@ var users = []
 var messages = []
 
 app.get("/", function(req, res){
-  res.render('index')
+  var user_index = getUserIndex(req.remoteAddress + req.remotePort)
+  
+  res.render('index', {
+    locals: {
+      "username" : users[user_index]
+    }
+  })
+})
+
+app.post("/say", express.bodyParser(), function(req, res){
+  console.log(req.body)
+  if (req.body && req.body.message) {
+    var user_index = getUserIndex(req.remoteAddress + req.remotePort)
+    
+    users[user_index].messages.push(req.body.message)
+    messages.push({ body: req.body.message, user: user_index })
+    
+    if (acceptsHtml(req.headers['accept'])){
+      res.redirect(302, '/')
+    } else {
+      res.send({ status: "ok", message: "mensaje guardado" })
+    }
+  } else {
+    res.send({ status: "nok", message: "fallo el envio del mensaje" })
+  }
 })
 
 app.listen(3000)
+
+// Helpers methods
+function acceptsHtml(header) {
+  var accepts = header.split(',')
+  
+  for (i = 0 ; i < accepts.length ; i ++) {
+    if (accepts[i] === 'text/html') { return true }
+  }
+  
+  return false
+}
+
+function getUserIndex(username){  
+  if (users.indexOf(username) == -1) {
+    users.push({ name: username, messages: [] })
+  }
+  
+  return users.indexOf(username)
+}
